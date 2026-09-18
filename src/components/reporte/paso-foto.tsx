06c1d24915a4ffe29af6885382ddec391678e0f4
spuchetti <spuchetti16@gmail.com>
2026-09-18 20:27:33 -0300

    fix: usar estilos compatibles con TypeScript de RN 0.86)

--- src/components/reporte/paso-foto.tsx
+++ src/components/reporte/paso-foto.tsx
@@ -247,7 +247,11 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   camaraWeb: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
     padding: 24,
